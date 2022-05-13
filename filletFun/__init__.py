84de5abe86b9da797912e14530e6f7f7c134917e
Aditya Nath <928.aditya@gmail.com>
2022-05-13 16:55:58 -0700

    Improving handling dependency install prompt

--- filletFun/__init__.py
+++ filletFun/__init__.py
@@ -7,113 +7,11 @@ from sys import exit
 
 try:
     import requests
-
-except ModuleNotFoundError as e:
-    print("\n[-] Error: {}".format(e))
-    print("[-] You are missing a required dependency.")
-    answer = input("\n[?] Do you wish to install the missing dependency? (y/n): ")
-    answer = answer.lower().strip()
-    if answer == "yes" or "y":
-        print("[!] Using pip3 to install module")
-        os.system("pip3 install requests")
-    if answer == "no" or "n":
-        print("[!] Please use pip3 to install module")
-
-except KeyboardInterrupt:
-        print("[!] Goodbye!")
-        exit()
-
-try:
     import argparse
-
-except ModuleNotFoundError as e:
-    print("\n[-] Error: {}".format(e))
-    print("[-] You are missing a required dependency.")
-    answer = input("\n[?] Do you wish to install the missing dependency? (y/n): ")
-    answer = answer.lower().strip()
-    if answer == "yes" or "y":
-        print("[!] Using pip3 to install module")
-        os.system("pip3 install argparse")
-    if answer == "no" or "n":
-        print("[!] Please use pip3 to install module")
-
-except KeyboardInterrupt:
-        print("[!] Goodbye!")
-        exit()
-
-try:
     from urllib.parse import urlparse
-
-except ModuleNotFoundError as e:
-    print("\n[-] Error: {}".format(e))
-    print("[-] You are missing a required dependency.")
-    answer = input("\n[?] Do you wish to install the missing dependency? (y/n): ")
-    answer = answer.lower().strip()
-    if answer == "yes" or "y":
-        print("[!] Using pip3 to install module")
-        os.system("pip3 install urllib")
-    if answer == "no" or "n":
-        print("[!] Please use pip3 to install module")
-
-except KeyboardInterrupt:
-        print("[!] Goodbye!")
-        exit()
-
-try:
     import re
-
-except ModuleNotFoundError as e:
-    print("\n[-] Error: {}".format(e))
-    print("[-] You are missing a required dependency.")
-    answer = input("\n[?] Do you wish to install the missing dependency? (y/n): ")
-    answer = answer.lower().strip()
-    if answer == "yes" or "y":
-        print("[!] Using pip3 to install module")
-        os.system("pip3 install re")
-    if answer == "no" or "n":
-        print("[!] Please use pip3 to install module")
-
-except KeyboardInterrupt:
-        print("[!] Goodbye!")
-        exit()
-
-try:
     from progress.bar import IncrementalBar
-
-except ModuleNotFoundError as e:
-    print("\n[-] Error: {}".format(e))
-    print("[-] You are missing a required dependency.")
-    answer = input("\n[?] Do you wish to install the missing dependency? (y/n): ")
-    answer = answer.lower().strip()
-    if answer == "yes" or "y":
-        print("[!] Using pip3 to install module")
-        os.system("pip3 install progress")
-    if answer == "no" or "n":
-        print("[!] Please use pip3 to install module")
-
-except KeyboardInterrupt:
-        print("[!] Goodbye!")
-        exit()
-
-try:
     from bs4 import BeautifulSoup
-
-except ModuleNotFoundError as e:
-    print("\n[-] Error: {}".format(e))
-    print("[-] You are missing a required dependency.")
-    answer = input("\n[?] Do you wish to install the missing dependency? (y/n): ")
-    answer = answer.lower().strip()
-    if answer == "yes" or "y":
-        print("[!] Using pip3 to install module")
-        os.system("pip3 install bs4")
-    if answer == "no" or "n":
-        print("[!] Please use pip3 to install module")
-
-except KeyboardInterrupt:
-        print("[!] Goodbye!")
-        exit()
-
-try:
     from ip2geotools.databases.noncommercial import DbIpCity
 
 except ModuleNotFoundError as e:
@@ -121,10 +19,10 @@ except ModuleNotFoundError as e:
     print("[-] You are missing a required dependency.")
     answer = input("\n[?] Do you wish to install the missing dependency? (y/n): ")
     answer = answer.lower().strip()
-    if answer == "yes" or "y":
+    if answer in ["yes", "y"]:
         print("[!] Using pip3 to install module")
-        os.system("pip3 install ip2geotools")
-    if answer == "no" or "n":
+        os.system("pip3 install -r ../requirements.txt")
+    else:
         print("[!] Please use pip3 to install module")
 
 except KeyboardInterrupt:
